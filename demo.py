6e79c4df5276af00705b8e18261ee648df83f5d6
sinchana s k <sinchusinchanask4@gmail.com>
2026-04-14 20:56:35 +0530

    first program

---
+++ demo.py
@@ -0,0 +1,12 @@
+"""print("this is the first program of my journey  in vs code ")
+print("Hello word");
+
+
+print("hello word")
+# print("4"+6)  # concatenation of string and number is not allowed 
+print(4+7)   # adding the two numbers are allowed 
+print("hi");  # ; is not considered
+ 
+print("hello word ","hi how are you " ,45)
+print(10>4)  # wwe can also inside the 
+ """
---
+++ variable.py
@@ -0,0 +1,9 @@
+a = 10
+b=10
+c=24
+a=c
+
+print(a,b,c)
+
+print(type(a),type(b),type(c),a,b,c)  # type function are used to check the data type of variable
+print(id(a),id(b),id(c))
