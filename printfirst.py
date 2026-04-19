32b2408f45dfeae6098c79159c8c4f5297dea150
sinchana s k <sinchusinchanask4@gmail.com>
2026-04-19 21:23:31 +0530

    working with variable and operators in python

---
+++ printfirst.py
@@ -0,0 +1,17 @@
+a = 10
+b=10
+c=24
+a=c
+
+print(a,b,c)
+
+print(type(a),type(b),type(c),a,b,c)  # type function are used to check the data type of variable
+print(id(a),id(b),id(c)) # id () function are used to check the address value of the variable . if the two variables are store the same value then the 
+#both variblae addreess will same  24 10 24
+#class 'int'> <class 'int'> <class 'int'> 24 10 24
+#140710122212504 140710122212056 140710122212504
+
+p="hello00"
+print(p)
+
+
---
+++ variable.py
@@ -0,0 +1,5 @@
+varr=5
+varr=4     #dynamic semntic 
+print(varr)  #ver is the build in function we cannot use as varivble name and also 35  kwywords are not use as varible  name 
+ef = "string"
+print(ef)
