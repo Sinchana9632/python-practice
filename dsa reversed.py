88f9b9a4b3bf2e640984658a8f62b893a0ca0f05
sinchana s k <sinchusinchanask4@gmail.com>
2026-04-20 23:09:42 +0530

    discussing about the list

---
+++ dsa reversed.py
@@ -0,0 +1,16 @@
+"""list1=[1,2,3]
+list2=[3,4,5]
+list3=list1+list2
+print(list3)
+
+list1=[2.0,4,"hello"]         # concatenation is done if also same data type occur in two list or differnt data types 
+list2=[3,4,5]
+list3=list1+list2
+print(list3)
+    """
+list=[1,2,3,4,5]
+print(list[1:4:1])  #[1,2,3,4]
+print(list[::-1])   #[5,4,3,2,1]
+print(list[1::-1])  #[2,1]
+print(list[:-3:-1])  #[5,4]
+print(list[-3::-1])   #[3,2,1]
