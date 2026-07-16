866aa509b9eb2fceb3aa7e7b0413803cd3ae72b8
sinchana s k <sinchusinchanask4@gmail.com>
2026-07-16 21:35:14 +0530

    changes

--- slicing.py
+++ slicing.py
@@ -12,4 +12,6 @@ else:
 #time complextity =o(n)  bcz if statement trvanser evry item on the lost 
 """
 list=[23,67,4,6,6,"sincaha"]
-print(list[3:1:-1])
+print(list[3:1:-1])  # step is goes from  right to left  means it start with inddex 3 and goes backwrad  right to left by yhe
+                       # neagtive  slicing  by  the default is -1 so it will   present 6 and 4 and stop for the index 2 
+                       # bcz stop will exclude  not include  and stop - start =number of  elemrs so 3-1 =2 elemnts 
