757fb8b21a6bcf0680ab835734d78de788629d1e
sinchana s k <sinchusinchanask4@gmail.com>
2026-08-12 22:27:18 +0530

    updated wprk

---
+++ 2D_Matrix.py
@@ -0,0 +1,8 @@
+grid=[
+    [1,2,3,4],
+    [4,6,7,8]
+]
+print(grid[0][1])   # it will print the value of the first row and second column  means 2
+print(len(grid))   # it will print the number of rows in the grid  means 2
+print(len(grid[0]))   # it will print the number of columns in the grid  means 4
+print(grid)
