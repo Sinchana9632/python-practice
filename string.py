dfcf86907d175ffdc63911946a5215e3f4c7c430
sinchana s k <sinchusinchanask4@gmail.com>
2026-08-20 22:13:11 +0530

    updated work

--- string.py
+++ string.py
@@ -1,4 +1,4 @@
-
+"""
 av="a"
 print(ord(av))
 print(chr(100))
@@ -6,4 +6,29 @@ print(chr(100))
 s1="sinchaan\n"
 s2="good Girls\n"
 s3=s1+s2  
-print(s3)
+print(s3) 
+
+   
+
+char_list=[]
+for i in ['h','l','o']:
+    char_list.append(i)
+    print(char_list)
+    print("".join(char_list))
+    """
+list=[1,3,4,5]
+for i in list:
+    if i==4:
+       continue
+    print([i])
+list=[1,3,4,5]
+for i in list:
+    if i==4:
+       break
+    print([i])
+    
+list=[1,3,4,5]
+for i in list:
+    if i==4:
+       pass   
+    print([i])
